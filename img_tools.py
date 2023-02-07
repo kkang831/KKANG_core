fe31191f8e6670c99cbec11aa6964734354ec2b0
Kyoungkook <kkang831@postech.ac.kr>
2023-02-07 01:05:44 +0000

    trivial

--- img_tools.py
+++ img_tools.py
@@ -9,84 +9,18 @@ import torch
 import numpy as np
 from PIL import Image
 
-#############################################
-# preprocess(images, channel_order='RGB', to_tensor)
-# input : numpy, np.uint8, 0~255, RGB, BHWC (HWC)
-# output : numpy, np.float32, -1~1, RGB, BCHW
-#############################################
-
-#############################################
-# postprocess(images)
-# input : tensor, -1~1, RGB, BCHW
-# output : np.uint8, 0~255, BGR, BHWC
-#############################################
-
-#############################################
-# Lanczos_resizing(image_target, resizing_tuple=(256,256))
-# input : -1~1, RGB, BCHW, Tensor
-# output : -1~1, RGB, BCHW, Tensor
-#############################################
-
-def preprocess(images, channel_order='RGB', to_tensor=False):
-    # input  : numpy, np.uint8, 0~255, RGB, (BHWC or HWC)
-    # output : numpy, np.float32, -1~1, RGB, BCHW
-	
-	max_val = 1.0
-	min_val = -1.0
-	
-	if len(images.shape) == 3:
-		images = images[np.newaxis, :]
-	
-	B, H, W, C = images.shape
-
-	if C == 3 and channel_order == 'BGR':
-		images = images[:,:,:,::-1]
-
-	images = images / 255.0 * (max_val - min_val) + min_val
-	images = images.astype(np.float32).transpose(0, 3, 1, 2)
-
-	if to_tensor:
-		images = torch.tensor(images)
-
-	return images
-
-def postprocess(images):
-	"""Post-processes images from `torch.Tensor` to `numpy.ndarray`."""
-	# input : tensor, -1~1, RGB, BCHW
-	# output : np.uint8, 0~255, BGR, BHWC
-
-	images = images.detach().cpu().numpy()
-	images = (images + 1.) * 255. / 2.
-	images = np.clip(images + 0.5, 0, 255).astype(np.uint8)
-	images = images.transpose(0, 2, 3, 1)[:,:,:,[2,1,0]]
-	return images
-
-def tensor2numpyimg(images):
-	"""Post-processes images from `torch.Tensor` to `numpy.ndarray`."""
-	# input : tensor, -1~1, RGB, BCHW
-	# output : np.uint8, 0~255, RGB, BHWC
-
-	shape = images.shape
-	images = images.detach().cpu().numpy()
-	images = (images + 1.) * 255. / 2.
-	images = np.clip(images + 0.5, 0, 255).astype(np.uint8)
-
-	images = images.transpose(0, 2, 3, 1)
-	if shape[1] == 1:
-		return images.squeeze(-1)
-	elif shape[1] == 3:
-		return images
-
+############
+# Preprocess
 def numpyimg2tensor(images, channel_order='RGB'):
-	"""Post-processes images from `torch.Tensor` to `numpy.ndarray`."""
+	"""Pre-processes images from `numpy.ndarray` to `torch.Tensor`."""
 	# input : np.uint8, 0~255, RGB, (BHWC or HWC)
-	# output: tensor, -1~1, RGB, BCHW 
+	# output: tensor, -1~1, RGB, BCHW
 
 	max_val = 1.0
 	min_val = -1.0
 	if len(images.shape) == 3:
 		images = images[np.newaxis, :]
-	
+
 	B, H, W, C = images.shape
 	if C == 3 and channel_order == 'BGR':
 		images = images[:,:,:,::-1]
@@ -95,19 +29,29 @@ def numpyimg2tensor(images, channel_order='RGB'):
 	images = images.astype(np.float32).transpose(0, 3, 1, 2)
 	return torch.tensor(images)
 
-def Lanczos_resizing(image_target, resizing_tuple=(256,256)):
-	# input : -1~1, RGB, BCHW, Tensor
-	# output : -1~1, RGB, BCHW, Tensor
-	image_target_resized = image_target.clone().cpu().numpy()
-	image_target_resized = (image_target_resized + 1.) * 255. / 2.
-	image_target_resized = np.clip(image_target_resized + 0.5, 0, 255).astype(np.uint8)
+def pilimg2tensor(images):
+	"""Pre-processes images from `PIL.Image` to `torch.Tensor`."""
+	# input : uint8, 0~255, RGB, HWC
+	# output: tensor, -1~1, RGB, BCHW
+	max_val = 1.0
+	min_val = -1.0
 
-	image_target_resized = image_target_resized.transpose(0, 2, 3, 1)
-	tmps = []
-	for i in range(image_target_resized.shape[0]):
-		tmp = image_target_resized[i]
-		tmp = Image.fromarray(tmp) # PIL, 0~255, uint8, RGB, HWC
-		tmp = np.array(tmp.resize(resizing_tuple, PIL.Image.LANCZOS))
-		tmp = torch.from_numpy(preprocess(tmp[np.newaxis,:])).cuda()
-		tmps.append(tmp)
-	return torch.cat(tmps, dim=0)
+	print('!!!!!!!!!!!!!!!!!')
+	print('not yet')
+	raise
+
+#############
+# Postprocess
+def tensor2numpyimg(input_tensor):
+	"""Post-processes images from `torch.Tensor` to `numpy.ndarray`."""
+	# input : tensor, -1~1, RGB, BCHW
+	# output : np.uint8, 0~255, RGB, BHWC
+	input_tensor = input_tensor.detach().cpu().numpy()
+	input_tensor = (input_tensor + 1.) * 255. / 2.
+	output_image = np.clip(input_tensor + 0.5, 0, 255).astype(np.uint8)
+
+	output_image = output_image.transpose(0, 2, 3, 1)
+	if output_image.shape[-1] == 1:
+		return output_image[:,:,:,0]
+	elif output_image.shape[-1] == 1:
+		return output_image
