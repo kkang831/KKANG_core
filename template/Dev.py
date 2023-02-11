2773fc3bdef918d6af7c18ee5ca8e44716c1933c
Kyoungkook <kkang831@postech.ac.kr>
2023-02-11 20:28:33 +0000

    trivial

---
+++ template/Dev.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os, sys, argparse, glob
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument(help='')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+if __name__ == '__main__':
+    main()
